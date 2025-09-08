3ab6011574a499a68a5d11216bf9a06d09480596
tommybrahhh <romanomaldito@gmail.com>
2025-09-08 18:17:15 +0200

    Updated

--- server.js
+++ server.js
@@ -16,8 +16,13 @@ const PORT = process.env.PORT || 3001;
 
 // Security middleware
 app.use(helmet());
+// Configure CORS dynamically based on environment
+const allowedOrigins = process.env.CORS_ORIGIN
+  ? process.env.CORS_ORIGIN.split(',')
+  : ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500'];
+
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500', 'http://127.0.0.1:5501'],
+    origin: allowedOrigins,
     credentials: true
 }));
 
